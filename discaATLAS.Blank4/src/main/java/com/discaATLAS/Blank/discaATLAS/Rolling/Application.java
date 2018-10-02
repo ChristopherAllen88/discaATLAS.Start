73e50f76ccb56952bbff0d89e66e88a76a26c1a2
Craig880 <38652551+Craig880@users.noreply.github.com>
2018-10-02 18:35:15 +0200

    First

---
+++ discaATLAS.Blank4/src/main/java/com/discaATLAS/Blank/discaATLAS/Rolling/Application.java
@@ -0,0 +1,12 @@
+package com.discaATLAS.Blank.discaATLAS.Rolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
